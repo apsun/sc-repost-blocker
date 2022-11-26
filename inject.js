52c0a6ceafa4d1bb30665223b5b16a834b98fa13
Andrew Sun <andrew@crossbowffs.com>
2022-11-26 11:35:01 -0800

    Fix usage of deprecated API

--- inject.js
+++ inject.js
@@ -2,7 +2,7 @@
 // are run with an isolated window object. This injects a
 // script tag into the page DOM (which we do have access to)
 // that points to the real content script.
-let file = chrome.extension.getURL("/content.js");
+let file = chrome.runtime.getURL("/content.js");
 let script = document.createElement("script");
 script.src = file;
 document.documentElement.appendChild(script);
