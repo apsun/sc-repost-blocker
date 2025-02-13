b81546674a0154a3e342bc8a8d1fbd30a49ed667
Andrew Sun <andrew@crossbowffs.com>
2025-02-12 21:13:13 -0800

    Run directly in page context, delete inject.js

--- inject.js
+++
@@ -1,8 +0,0 @@
-// Content script loader, because normally content scripts
-// are run with an isolated window object. This injects a
-// script tag into the page DOM (which we do have access to)
-// that points to the real content script.
-let file = chrome.runtime.getURL("/content.js");
-let script = document.createElement("script");
-script.src = file;
-document.documentElement.appendChild(script);
